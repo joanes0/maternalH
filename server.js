cc6d1de1f00902ad528360fd8f20331fa11995fb
Joanes <joanes@epa.local>
2023-09-05 19:07:50 +0300

    midleware

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json()); 
 app.use(express.urlencoded());
-app.use(express.static('public'));
+app.use(express.static('maternalH'));
 
 
 //// API ROUTES
@@ -23,7 +23,7 @@ app.get('/', (req, res)=>{
 app.post('/', (req, res)=>{
     console.log(req.body);
 
-  res.sendFile(__dirname+ '/final.html');
+  res.sendFile(__dirname+ '/public/final.html');
 });
 
 
